064c53f30f092167b4cb5b25db3aac44e990194f
ruslanmurzakaev.re2964@outlook.com <ruslanmurzakaev.re2964@outlook.com>
2025-01-25 19:52:05 +0500

    задача 2 выполнена

--- Task_02_01/Program.cs
+++ Task_02_01/Program.cs
@@ -4,11 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int a = 8;
-            int b = 14;
-            object c = 0,78539;
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
 
+            double action_1 = Math.Pow(a - 1, 1.0 / 3.0);
+            double action_2 = Math.Pow(b + action_1, 1.0 / 4.0);
+            double action_3 = Math.Abs(a - b);
+            double action_4 = Math.Sin(Math.Pow(c, 2)) + Math.Tan(c);
+            double action_5 = action_3 * action_4;
+            double action_6 = action_2 / action_5; //ответ 
 
+            Console.WriteLine(action_6);
         }
     }
 }
